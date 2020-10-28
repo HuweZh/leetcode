43feac1f49327530c046de124321735b10ef4d5f
huhusw <1241671281@qq.com>
2020-10-28 23:53:19 +0800

    第一次提交

---
+++ src/main/com/huhusw/M144.java
@@ -0,0 +1,45 @@
+package com.huhusw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class M144 {
+
+    // 二叉树前序遍历
+    public static void main(String[] args) {
+
+    }
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null){
+            return result;
+        }
+        result.add(root.val);
+        if(root.left != null) {
+            preorderTraversal(root.left);
+        }
+        if(root.right != null){
+            preorderTraversal(root.right);
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
