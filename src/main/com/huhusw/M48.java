bc12f0da7d65f13a750647eb73e345bad66b9f40
“e <1241671281@qq.com>
2020-12-22 00:18:42 +0800

    提交

---
+++ src/main/com/huhusw/M48.java
@@ -0,0 +1,35 @@
+package com.huhusw;
+
+public class M48 {
+    public static void main(String[] args) {
+        M48 m48 = new M48();
+        int[][] matrix = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        m48.rotate(matrix);
+        System.out.println("nihao");
+    }
+
+public void rotate(int[][] matrix) {
+    int length = matrix.length;
+    // 对角线反转
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < i; j++) {
+//                if (i == j) continue;
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    //镜像反转
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][length - j - 1];
+            matrix[i][length - j - 1] = temp;
+        }
+    }
+}
+}
---
+++ src/main/com/huhusw/S746.java
@@ -0,0 +1,18 @@
+package com.huhusw;
+
+/**
+ * @author huhusw
+ * @Description
+ * @create 2020-12-21 23:34
+ */
+public class S746 {
+public int minCostClimbingStairs(int[] cost) {
+    int[] dp = new int[cost.length + 1];
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 2; i < cost.length + 1; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    return dp[cost.length];
+}
+}
