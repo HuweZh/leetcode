26cf064f699efef549b6077982d7b2e8f7f07a17
“e <1241671281@qq.com>
2021-04-25 14:54:14 +0800

    不知道第几次提交

---
+++ src/main/com/huhusw/M103.java
@@ -0,0 +1,53 @@
+package com.huhusw;
+
+import java.util.*;
+
+/**
+ * @author huhusw
+ * @Description
+ * @create 2020-12-22 23:53
+ */
+
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
+
+public class M103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        boolean flag = false;
+        Deque<TreeNode> temp = new LinkedList<TreeNode>();
+        temp.add(root);
+        while (temp != null) {
+            int size = temp.size();
+            List<Integer> stem = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                //从左到右
+                if (flag) {
+                    TreeNode treeNode = temp.pollLast();
+                    if(treeNode.left!=null){
+                        stem.add(treeNode.left.val);
+
+                    }
+                }
+                //从右到左
+                else {
+                    TreeNode treeNode = temp.pollFirst();
+                }
+            }
+        }
+        return null;
+    }
+}
