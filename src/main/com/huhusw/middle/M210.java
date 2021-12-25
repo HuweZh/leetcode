64eee865d9bfc0e585f1dba877a1b66a1c95c96f
HuweZh <1241671281@qq.com>
2021-12-25 13:23:17 +0800

    210 dfs+bfs

--- src/main/com/huhusw/middle/M210.java
+++ src/main/com/huhusw/middle/M210.java
@@ -2,11 +2,18 @@ package com.huhusw.middle;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 课程表2，返回一个符合条件的课程顺序
  */
 public class M210 {
+    /*
+    深度优先遍历 拓扑排序
+    使用栈来记录拓扑排序，栈底元素的先修课程要求最高，栈顶最小
+    从栈顶到栈底的排序就是符合题意的排序
+     */
+
     //邻接表存储图
     List<Integer>[] graph;
     //将课程当做节点，判断节点是否访问过
@@ -81,5 +88,80 @@ public class M210 {
         res[curIndex--] = index;
         //修改状态为已完成访问
         visited[index] = 2;
+    }
+
+    /*
+       广度优先遍历，拓扑排序
+       使用队列进行模拟，利用节点的入度来计算是否入队列
+       入度为0的节点代表修课程的前置条件都已经满足
+     */
+    //邻接表
+    List<Integer>[] graphBFS;
+    //每个节点的入度
+    int[] inDegree;
+    //结果数组
+    int[] resBFS;
+    //结果数组的索引
+    int curIndexBFS;
+
+    /**
+     * 广度优先遍历，找到符合题意的课程学习顺序
+     *
+     * @param numCourses    课程数量
+     * @param prerequisites 先修课程要求
+     * @return 符合题意的修课顺序
+     */
+    public int[] findOrder2(int numCourses, int[][] prerequisites) {
+        //初始化
+        inDegree = new int[numCourses];
+        resBFS = new int[numCourses];
+        curIndex = 0;
+        //构建邻接表
+        graphBFS = new LinkedList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graphBFS[i] = new LinkedList<Integer>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            int from = prerequisites[i][1];
+            int to = prerequisites[i][0];
+            graphBFS[from].add(to);
+            //记录入度
+            inDegree[to]++;
+        }
+        //广度优先搜索
+        bfs();
+        //全部节点都已经遍历，返回
+        if (curIndexBFS == numCourses) {
+            return resBFS;
+        }
+        //否则不存在满足题意的顺序
+        return new int[0];
+    }
+
+    /**
+     * 深度优先搜索
+     */
+    public void bfs() {
+        //队列中存储着入度为0的节点，这些节点是可以直接进行学习的
+        Queue<Integer> queue = new LinkedList<>();
+        //初始化队列
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        //进行学习
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            resBFS[curIndexBFS++] = x;
+            for (int neighbor : graphBFS[x]) {
+                //相应的邻居的入度都减1
+                inDegree[neighbor]--;
+                //并更新队列
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
     }
 }
