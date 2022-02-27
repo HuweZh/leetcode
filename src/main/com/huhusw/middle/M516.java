5b26df1f89030de5cd3f782e4c5baf96b9e2976f
HuweZh <1241671281@qq.com>
2022-02-27 14:40:31 +0800

    516 最长回文子序列 动态规划状态压缩

--- src/main/com/huhusw/middle/M516.java
+++ src/main/com/huhusw/middle/M516.java
@@ -1,5 +1,6 @@
 package com.huhusw.middle;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class M516 {
@@ -8,6 +9,7 @@ public class M516 {
         int bbbab = m516.longestPalindromeSubseq("bbbab");
         System.out.println(bbbab);
     }
+
     //动态规划
     //设dp[i][j]为原字符串索引为i和j之间的最长回文子序列，设0<=i<=j<n
     //则状态dp[0][n-1]即为所求
@@ -34,5 +36,35 @@ public class M516 {
             }
         }
         return dp[0][n - 1];
+    }
+
+    /**
+     * 状态压缩
+     *
+     * @param s 字符串
+     * @return 最长回文子序列
+     */
+    public int longestPalindromeSubseq2(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        //注意循环顺序
+        for (int i = n - 1; i >= 0; i--) {
+            //初始状态
+            int pre = 0;
+            char c1 = s.charAt(i);
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+                char c2 = s.charAt(j);
+                //状态转移
+                if (c1 == c2) {
+                    dp[j] = pre + 2;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                pre = temp;
+            }
+        }
+        return dp[n - 1];
     }
 }
