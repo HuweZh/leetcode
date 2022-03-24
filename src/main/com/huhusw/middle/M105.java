bce2fdbb361655125440e02619b792ecb3089e6e
HuweZh <1241671281@qq.com>
2022-03-24 11:15:42 +0800

    105 前序中序遍历构造二叉树 数组切片

--- src/main/com/huhusw/middle/M105.java
+++ src/main/com/huhusw/middle/M105.java
@@ -2,6 +2,8 @@ package com.huhusw.middle;
 
 import com.huhusw.TreeNode;
 
+import java.util.Arrays;
+
 public class M105 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
@@ -11,11 +13,11 @@ public class M105 {
      * 根据前序遍历和中序遍历确定一颗二叉树
      *
      * @param preorder 前序遍历数组
-     * @param preStart    前序遍历数组的下界
-     * @param preEnd    前序遍历数组的上界
+     * @param preStart 前序遍历数组的下界
+     * @param preEnd   前序遍历数组的上界
      * @param inorder  中序遍历数组
-     * @param inStart     中序遍历数组的下界
-     * @param inEnd     中序遍历数组的上界
+     * @param inStart  中序遍历数组的下界
+     * @param inEnd    中序遍历数组的上界
      * @return 构造的树
      */
     private TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
@@ -38,6 +40,35 @@ public class M105 {
         int leftSize = index - inStart;
         root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
         root.right = build(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, inEnd);
+        return root;
+    }
+
+
+    /**
+     * 需要注意的是，在数组切片过程中，左右子树的节点个数
+     * 切片的运行时间大于不切片2倍多
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+        if (inorder.length == 1) {
+            return new TreeNode(inorder[0]);
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        int index = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == preorder[0]) {
+                index = i;
+                break;
+            }
+        }
+        root.left = buildTree2(Arrays.copyOfRange(preorder, 1, 1 + index), Arrays.copyOfRange(inorder, 0, index));
+        root.right = buildTree2(Arrays.copyOfRange(preorder, 1 + index, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
         return root;
     }
 }
