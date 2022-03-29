e84992f065d95e718fac86f75e6c84f73e907619
HuweZh <1241671281@qq.com>
2022-03-29 10:22:07 +0800

    669 二叉搜索树剪枝 第二次尝试

--- src/main/com/huhusw/middle/M669.java
+++ src/main/com/huhusw/middle/M669.java
@@ -39,4 +39,39 @@ public class M669 {
         //返回剪枝后的树
         return root;
     }
+
+
+    /**
+     * 第二次尝试二叉树剪枝
+     *
+     * @param root 根结点
+     * @param low  下界
+     * @param high 上界
+     * @return 剪枝后的二叉搜索树
+     */
+    public TreeNode trimBST2(TreeNode root, int low, int high) {
+        //当前节点为空，直接返回空
+        if (root == null) {
+            return null;
+        }
+        //当前节点大于上界，说明当前节点的右子树全部剪掉
+        if (root.val > high) {
+            //切换当前节点为左子树
+            root = root.left;
+            //继续遍历左子树
+            root = trimBST(root, low, high);
+        } else if (root.val < low) {
+            //当前节点小于下界，左子树全部剪掉
+            //切换到右子树
+            root = root.right;
+            //继续遍历右子树
+            root = trimBST(root, low, high);
+        } else {
+            //否则正常遍历左右子树
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        //返回剪枝后的树
+        return root;
+    }
 }
