d8f4a6dad4b1ad89c90209893512f482b99c8b8a
HuweZh <1241671281@qq.com>
2022-04-14 11:18:18 +0800

    剑指Offer15 计算二进制中1的个数 逻辑右移

--- src/main/com/huhusw/sword/S15.java
+++ src/main/com/huhusw/sword/S15.java
@@ -7,7 +7,7 @@ package com.huhusw.sword;
  */
 public class S15 {
     // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
+    public int hammingWeight2(int n) {
         int count = 0;
         int index = 0;
         while (index != 32) {
@@ -15,6 +15,24 @@ public class S15 {
             n = n >> 1;
             index++;
         }
+        return count;
+    }
+
+    /**
+     * 计算1的个数
+     * @param n
+     * @return
+     */
+    public int hammingWeight(int n) {
+        //结果
+        int count = 0;
+        while (n != 0) {
+            //每次与1
+            count += n & 1;
+            //逻辑右移
+            n = n >>> 1;
+        }
+        //返回结果
         return count;
     }
 }
