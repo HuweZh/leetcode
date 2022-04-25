4550e42987abe4b513c0c70eeee1a96e066a44a3
HuweZh <1241671281@qq.com>
2022-04-25 10:46:24 +0800

    398 随机返回索引 池塘抽样

--- src/main/com/huhusw/middle/M398.java
+++ src/main/com/huhusw/middle/M398.java
@@ -45,4 +45,48 @@ public class M398 {
         //列表中随机返回一个
         return stem.get(random.nextInt(stem.size()));
     }
+
+
+    /**
+     * 池塘抽样
+     * 在一个无限的序列中，遍历一遍以特定概率找到特定值对应的索引
+     */
+    //数组
+    int[] arr;
+
+    public void solution(int[] nums) {
+        //初始化
+        arr = nums;
+        random = new Random();
+    }
+
+    /**
+     * 池塘抽样
+     * 保证返回特定值对应的索引是概率相同的
+     *
+     * @param target 特定值
+     * @return 对应的索引
+     */
+    public int pick2(int target) {
+        //记录序列中有多少个与target相等
+        int count = 0;
+        //返回值
+        int res = -1;
+        //遍历数组
+        for (int i = 0; i < arr.length; i++) {
+            //找到一个相等的值，需要进行状态更新
+            if (arr[i] == target) {
+                //更新计数
+                count++;
+                //在[0,count)中选择一个整数，每个整数的概率都是1/count
+                //让其等于其中一个最特殊的整数0，其概率为1/count，且保证只有一个元素相等时，一定会有结果
+                //所以，res是以1/count的概率进行更新，直到找到所有的target
+                //这时候对应的索引就是1/count的概率出现的
+                if (random.nextInt(count) == 0) {
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
 }
