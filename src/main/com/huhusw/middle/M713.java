58e040af2fee530155a0a65749c430d931304da0
HuweZh <1241671281@qq.com>
2022-05-05 10:53:59 +0800

    713 乘积小于k的子数组 二分查找

--- src/main/com/huhusw/middle/M713.java
+++ src/main/com/huhusw/middle/M713.java
@@ -72,4 +72,59 @@ public class M713 {
         }
         return res;
     }
+
+    /**
+     * 二分查找
+     * 对于连续子数组的乘积，严格小于k这个不等式来说
+     * 两边同时取对数，就变成了连续子数组的和严格小于log(k)
+     * 而且，数组元素都是正数，前缀和是一个递增数组
+     * 对于区间[i,j]来说，找到最小满足题意的i=l
+     * 那么就是[l,j]满足题意，计算其方案数
+     *
+     * @param nums 数组
+     * @param k    阈值
+     * @return 方案数
+     */
+    public int numSubarrayProductLessThanK3(int[] nums, int k) {
+        //结果
+        int res = 0;
+        //定义前缀和数组
+        int n = nums.length;
+        double[] logPreSum = new double[n + 1];
+        //计算前缀和
+        for (int i = 0; i < n; i++) {
+            logPreSum[i + 1] = logPreSum[i] + Math.log(nums[i]);
+        }
+        //计算阈值
+        double logk = Math.log(k);
+        //遍历数组，即设定右端点
+        for (int i = 0; i < n; i++) {
+            //左右端点
+            int l = 0;
+            int r = i + 1;
+            //index就是在[i,j]区间内满足题意的最小值
+            int index = i + 1;
+            //sum_j-sum_l<logk=>sum_l>sum_j-logk
+            //所以求得l的方式变为一个不等式，不等式右边的值为val
+            //但是由于double的精度问题，加上一个1e-10以防万一
+            double val = logPreSum[index] - logk + 1e-10;
+            //二分查找
+            while (l <= r) {
+                //中间值
+                int mid = l + (r - l) / 2;
+                //找l，找到最小的l
+                //当前已经满足题意，继续向左半部分寻找
+                if (logPreSum[mid] > val) {
+                    r = mid - 1;
+                    index = mid;
+                } else {
+                    //右半部分
+                    l = mid + 1;
+                }
+            }
+            //加上当前区间的方案数
+            res += i + 1 - index;
+        }
+        return res;
+    }
 }
