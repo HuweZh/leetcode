37276f002c6173606f345eb8e2964c804430a1f4
HuweZh <1241671281@qq.com>
2022-05-13 12:17:55 +0800

    300 最长递增子序列 贪心+二分

--- src/main/com/huhusw/middle/M300.java
+++ src/main/com/huhusw/middle/M300.java
@@ -38,4 +38,59 @@ public class M300 {
         }
         return res;
     }
+
+    /**
+     * 贪心+二分求最长递增子序列
+     * dp[i]表示长度为i的序列中第i位的最小值
+     * 这样表示，dp[1]表示的就是长度为1的子序列，最小值为dp[1]
+     * 于是dp数组是一个递增的数组，可以利用二分查找出最后一个小于元素值的索引，并更新
+     * 对于大于dp最后一个元素的值，直接拼接在后面
+     * 对于小于dp最后一个元素的值，二分查找一个位置更新
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        //边界情况
+        if (nums.length == 0) {
+            return 0;
+        }
+        //dp数组，贪心，dp[i]表示长度为i的序列中第i位的最小值
+        int[] dp = new int[nums.length + 1];
+        //初始值
+        dp[1] = nums[0];
+        //dp数组中的元素个数
+        int len = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //当前元素大于dp数组的最大元素，直接加入
+            if (nums[i] > dp[len]) {
+                dp[++len] = nums[i];
+            } else {
+                //二分查找一个位置，为nums[i]分配
+                int l = 1;
+                int r = len;
+                //最后一个小于nums[i]的索引
+                //初始值为0，要是没有找到该位置，就放在第一位
+                int pos = 0;
+                //二分
+                while (l <= r) {
+                    //中间
+                    int mid = (l + r) >> 1;
+                    //中间值小于当前元素，提升左边界并更新pos
+                    //否则，更新右边界
+                    if (dp[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                //pos指示最后一个小于nums[i]的索引
+                //将下一个元素进行更新
+                dp[pos + 1] = nums[i];
+            }
+        }
+        //返回最长长度
+        return len;
+    }
 }
